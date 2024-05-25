4b114591125eb798838dc8fc4aef8d341a7922ed
5HoSaZ <ttzoththd@gmail.com>
2024-05-25 15:27:29 +0700

    new file:   modules/voice_input/recording.py 	new file:   notebooks/mic.ipynb

---
+++ modules/voice_input/recording.py
@@ -0,0 +1,41 @@
+import pyaudio
+import numpy as np
+
+FRAMES_PER_BUFFER = 3200
+FORMAT = pyaudio.paInt16
+CHANNELS = 1
+RATE = 16000
+audio = pyaudio.PyAudio()
+
+def record_audio():
+    stream = audio.open(
+        format=FORMAT,
+        channels=CHANNELS,
+        rate=RATE,
+        input=True,
+        input_device_index = 0,
+        frames_per_buffer=FRAMES_PER_BUFFER,
+    )
+
+    print("Start recording")
+
+    frames = []
+    seconds = 1
+    for _ in range(int(RATE/ FRAMES_PER_BUFFER + seconds)):
+        data = stream.read(FRAMES_PER_BUFFER)
+        frames.append(data)
+
+
+    print("Recording stopped")
+
+    stream.stop_stream()
+    stream.close()
+
+    return np.frombuffer(b"".join(frames), dtype="int16")
+
+def terminate():
+    audio.terminate()
+
+if __name__ == '__main__':
+    record_audio()
+    terminate()
